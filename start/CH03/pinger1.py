e83950d0f1c4c30ade953a983691aba69f72ec4f
Melinda <henkemelinda098.gmail.com>
2025-10-08 08:29:56 -0700

    Made multiplatform

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -4,13 +4,26 @@
 
 #Import things
 import os
+import platform
 
 #Assign Ip to variable 
-ip_addr = "127.0.0.1"
+ip_addr = "169.254.1.1"
 
+#Find current OS
+current_os =platform.system().lower()
 #Build ping command
-ping_cm = "ping -c 1 -W 2  " + ip_addr + " > /dev/null 2>&1"
+# #If Windows
+if current_os == "windows" :
+    ping_cmd = "ping -n 1 -w 2 ip_addr >nul"
+else:ping_cmd = "ping -c 1 -W 2  " + ip_addr + " > /dev/null 2>&1"
+    ping_cmd = "ping -n -w 2 ip_addr>nul"
+else:ping_cmd = "ping -c 1 -W 2  " + ip_addr + " > /dev/null 2>&1"
+
+
+
+    
 print (ping_cmd)
+
 #Execute ping command
 exit_code= os.system ( ping_cm)
 #print results
@@ -19,3 +32,5 @@ if exit_code == 0:
     print (f"{ip_addr} is online")
 else:
     print (f"{ip_addr} is NOT online")
+
+#
