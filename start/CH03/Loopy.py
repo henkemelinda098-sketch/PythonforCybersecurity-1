2ea3a606e27bc218f19e146baab7d38eff1ef3b3
Melinda <henkemelinda098.gmail.com>
2025-10-08 08:51:41 -0700

    Started Loopy

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+
+def hi ():
+    for i in range (8):
+        print ("Hello World")
+        print(i)
+
+print ("starting")
+hi ()
+print ("done")
