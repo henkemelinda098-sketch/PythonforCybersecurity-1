31a42fe9d2b6056557d29cead79f945b0b740c31
Melinda <henkemelinda098.gmail.com>
2025-10-12 22:49:30 -0700

    modified conditions

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -3,8 +3,11 @@
 #By Melinda Henke 10/10
 
 #ask the user if today is a good day
-answer = input ("Is today a good? (y/n)")
+answer = input ("Is today a good day? (y/n)")
 
 #check the user's response 
-if answer == "y":
-    print ("Yes it is")
+if answer.lower() == "y":
+
+#use a loop to print the message 10 times
+    for i in range (10):
+        print("Yeah it is")
