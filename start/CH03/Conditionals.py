9e10998d480a4b02b2be9bd20edd4a7be45ccc97
Melinda <henkemelinda098.gmail.com>
2025-10-12 22:18:59 -0700

    Conditionals

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By 
+#By Melinda Henke 10/10
+
+#ask the user if today is a good day
+answer = input ("Is today a good? (y/n)")
+
+#check the user's response 
+if answer == "y":
+    print ("Yes it is")
