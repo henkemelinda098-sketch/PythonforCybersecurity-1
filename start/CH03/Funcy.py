3fdd7128f5ecf805c1b2285297bfacc0f42194c4
Melinda <henkemelinda098.gmail.com>
2025-10-15 08:48:43 -0700

    functions

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
 # example workign with Functions
 #By 
+
+# basic function example
+def func1():
+    print ("I am a function")
+
+# Function that adds 2 numbers
+def add_2_nums(num_one, num_two):
+    num_one=int(num_one)
+    num_two= int(num_two)
+    
+    print (num_one + num_two)
+
+#Function that does exponents
+def exponent (number, power=2):
+    print ( number **power)
+
+#Function that returns a value
+def cube_number (number_one):
+    value = number_one **3
+    return value
+
+func1()
+add_2_nums (5,7)
+add_2_nums (num_one=9, num_two=3)
+add_2_nums (num_two=15, num_one=6)
+exponent(7,4)
+exponent (3)
+return_val = cube_number(5)
+print (return_val)
+
+add_2_nums("5", 6)
--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -6,6 +6,20 @@
 import os
 import platform
 
+def ping_address (ip_address):
+    #Find current OS
+    current_os =platform.system().lower()
+    #Build ping command
+    #If Windows
+    if current_os == "windows" :
+        ping_cmd = "ping -n 1 -w 2 ip_address >nul"
+    else:
+        ping_cmd = "ping -c -w 2" + ip_address + ">/dev/nul 2>&1"
+
+    #Execute ping command
+    exit_code= os.system ( ping_cmd)    
+    return_exit_code
+
 #Assign Ip to variable 
 ip_prefix = "192.168.0."
 
@@ -14,6 +28,9 @@ for final_octet in range (254):
     #Buiild IP address
     ip_addr = ip_prefix + str( final_octet+1)
     
+    #call function
+    exit_code = ping_address (ip_addr)
+
     #Find current OS
     current_os =platform.system().lower()
     #Build ping command
@@ -22,8 +39,6 @@ for final_octet in range (254):
         ping_cmd = "ping -n 1 -w 2 ip_addr >nul"
     else:
         ping_cmd = "ping -c -w 2" + ip_addr + ">/dev/nul 2>&1"
-
-
 
 #Execute ping command
 exit_code= os.system ( ping_cmd)
