eb158a590127b2ac13da937e285a6750f4c715ff
Melinda <henkemelinda098.gmail.com>
2025-10-20 09:46:17 -0700

    Encrypt

--- start/CH05/ASCIIgen.py
+++ start/CH05/ASCIIgen.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 # ASCII generator
 # Uses chr() to create ASCII characters
-# By 
+# By Melinda Henke 10/20
+
+#create loop 0-127
+for num in range (128):
+    # print value and chr () output
+    print (f"{num} '{chr(num)}'")
--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By Melinda Henke 10/20
+
+
+# Get message from user
+message = input("What is the message: ")
+message = message.upper ()
+final_message = ""
+
+#For each letter in message
+for letter in message:
+    #Convert letter in message
+    converted_letter =ord (letter)
+   
+    #Check for letter
+    if converted_letter >= 65 and converted_letter <= 90:
+        #Add 13 to number
+        converted_letter += 13
+        #Past Z?
+        if converted_letter > 90: 
+            #Substract 26 from number
+            converted_letter -= 26
+        #Convert number to letter
+        upt_letter = chr (converted_letter)
+        final_message += upt_letter
+    else: 
+        final_message += letter
+
+#Print updated message
+print (final_message)
