28d1e8688cee3eff07024a6468b2a3c1217acfb4
Melinda <henkemelinda098.gmail.com>
2025-10-19 22:44:08 -0700

    function

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -2,12 +2,15 @@
 # example workign with conditionals
 #By Melinda Henke 10/10
 
+#def function
+def send_message():
+    # use a loop to print the message
+    for i in range (10):
+        print ("Yeah it is")
+
 #ask the user if today is a good day
 answer = input ("Is today a good day? (y/n)")
 
 #check the user's response 
-if answer.lower() == "y":
-
-#use a loop to print the message 10 times
-    for i in range (10):
-        print("Yeah it is")
+if answer== "y": 
+    send_message ()
