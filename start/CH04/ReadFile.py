abc2bb6d21f81670044c53e17013e7698b850c07
Melinda <henkemelinda098.gmail.com>
2025-10-19 17:27:44 -0700

    created text file

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
 # By 
+
+
+import os
+# get current directory
+script_path = os.path.abspath (__file__)
+script_dir = os.path.dirname (script_path)
+# build file path
+file_path = os.path.join (script_dir, "testfile1.txt")
+
+
+# create file object
+f = open( file_path, "r")
+
+# read file object
+print (f.read())
+
+#close file object
+f.close()
--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
 # By 
+#10/15
+
+import os
+# get current directory
+script_path = os.path.abspath (__file__)
+script_dir = os.path.dirname (script_path)
+# build file path
+file_path = os.path.join (script_dir, "testfile.txt")
+
+
+# create file object
+f = open( file_path, "w")
+
+#write to file object
+f.write ("My name is Ed\n ")
+f.write(" I like rubber ducks ")
+f.write (" Hello World\n ")
+
+#close file object
+f.close()
