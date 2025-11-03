7dd5956818b1634170db0f0818696416ac62eae3
Melinda <henkemelinda098.gmail.com>
2025-11-03 09:23:54 -0800

    search 404

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Melinda 11/3
+import os
+
+# Ask which file to open
+log_file = input ("Which file to analyze? ")
+
+# get current directory
+script_path = os.path.abspath (__file__)
+script_dir = os.path.dirname (script_path)
+# build file path
+file_path = os.path.join (script_dir, "testfile1f.txt")
+
+# open log file
+f = open (file_path, "r")
+
+#Read the file line by line
+while True:
+    line = f.readline()
+    if not line:
+        break
+    # look for 404
+    if "404" in line:
+        # print line
+        print(line)
+
+# Close file
+f.close()
