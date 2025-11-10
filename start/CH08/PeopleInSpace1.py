6a5a80894aca38eb5c22ecad02a1eb41cc130ed0
Melinda <henkemelinda098.gmail.com>
2025-11-10 09:44:13 -0800

    ummmm

--- start/CH08/PeopleInSpace1.py
+++ start/CH08/PeopleInSpace1.py
@@ -1,1 +1,34 @@
-# paste code here
+import requests
+
+url = "http://api.open-notify.org/astros.json"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+#Convert number of people
+astros = response.json()
+
+# Print number of people
+print (f"There are currently {astros}['number]} people in space") 
+
+#First person in space
+print("The first person in space is {0}, on craft {1}".format{astros]["people"][0]["name"], astros ["people"][0]["craft"]"craft))
+    astros ["people"][0]["name"],
+    astros ["people"][0]["craft"])
+    )
+# Second person in space
+second_person = astros ["people"][1]
+print ("The second person in space is {0}, on craft {1}".format ()
+    second_person["name"],
+    second_person["craft"]
+))
+
+
+# Loop throug all astronauts
+for person in astros ["person"]:
+    print ("{0} is on {1}".format(
+        person ["name"],
+        person ["craft"]
+    ))
